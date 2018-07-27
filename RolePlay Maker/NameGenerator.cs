9daef214197282c638aa0d243d362ee7fa99c2ca
Stalkash <stalkach86@gmail.com>
2018-07-28 02:37:19 +0500

    Add a name generator

---
+++ RolePlay Maker/NameGenerator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RolePlay_Maker
+{
+    class NameGenerator
+    {
+        private static readonly string[] names =
+        {
+            "Вайлит",
+            "Шеф-Шеф",
+            "Драйвер Нефи",
+            "Дюк",
+            "Джексон",
+            "Антони",
+            "Эмма",
+            "Линда",
+            "Маргарет",
+            "Томас",
+            "Патрисия",
+            "Лиза",
+            "Дэвид",
+            "Дональд",
+            "Кристофер",
+            "Шэрон",
+            "Стивен",
+            "Джэк",
+            //TODO: Add more names;
+        };
+        private static readonly string[] surnames =
+        {
+            "",
+            //TODO: Add more surnames
+        };
+
+        public static string Generate(string Fraction = "")
+        {
+            string name = names[new Random().Next(0, names.Length)];
+            string surname = surnames[new Random().Next(0, surnames.Length)];
+
+            return Fraction + name + " " + surname;
+        }
+    }
+}
